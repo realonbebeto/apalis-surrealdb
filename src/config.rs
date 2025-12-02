c61971494f826830c0e71973bfe15d790669e38b
Bebeto Nyamwamu <projobs254@gmail.com>
2025-12-02 16:41:03 -0500

    ci fixes

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-use apalis_core::backend::{queue::Queue, BackendExt, ConfigExt};
+use apalis_core::backend::{BackendExt, ConfigExt, queue::Queue};
 use ulid::Ulid;
 
 use crate::{CompactType, SurrealContext, SurrealStorage};
@@ -6,11 +6,11 @@ use crate::{CompactType, SurrealContext, SurrealStorage};
 impl<Args: Sync, D, F> ConfigExt for SurrealStorage<Args, D, F>
 where
     Self: BackendExt<
-        Context = SurrealContext,
-        Compact = CompactType,
-        IdType = Ulid,
-        Error = surrealdb::Error,
-    >,
+            Context = SurrealContext,
+            Compact = CompactType,
+            IdType = Ulid,
+            Error = surrealdb::Error,
+        >,
 {
     fn get_queue(&self) -> Queue {
         self.config.queue().clone()
